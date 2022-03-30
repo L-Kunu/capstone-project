6f34678f7e8088f90326ba93f7dc25698b283699
L-Kunu <lillykunu>
2022-03-30 10:32:02 +0200

    In process/ issues with nested arrays in Database.js

---
+++ src/components/Database.js
@@ -0,0 +1,34 @@
+export const List = [
+    {
+        id: 0,
+        name:'Saeed Jaberi',
+        type: immigration,
+        language:[Persian, English, Chinese],
+        location: 'Hamburg',
+        contact: {
+            address: 'Große Reichenstraße 27, 20457 Hamburg',
+            telephone: '040 413 499 87',
+            email: 'jaberi@jaberilawyer.com',
+            website: 'jaberilawyer.com',
+            business hours: ['Mon - Thu: 9am - 6pm', 
+                            'Fri: 9am - 4pm'], 
+
+        },
+
+    },
+
+    {
+        id: 0,
+        name:'Kadir Katran',
+        type: immigration,
+        language:[Turkish],
+        location: 'Hamburg',
+        contact: {
+            address: 'Kroonhorst 9D, 22549 Hamburg',
+            telephone: '040 244 333 14',
+            email: 'jaberi@jaberilawyer.com',
+            website: 'jaberilawyer.com'
+        },
+
+    },
+]
